28e72dce624b61da74394f10afefdef9457f6c58
Porohnya <90443640+Porohnya@users.noreply.github.com>
2022-04-29 22:59:06 +0300

    Update login.php

--- 5/login.php
+++ 5/login.php
@@ -34,9 +34,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         form {
             display: flex;
             flex-direction: column;
-            width: 160px;
+            width: 220px;
             margin: 80px auto;
-            padding: 2px 3px;
+            padding: 5px;
             border-radius: 5px;
             background-color: rgba(255, 255, 255, 0.4);
             box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.4);
